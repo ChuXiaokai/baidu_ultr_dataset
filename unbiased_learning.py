38c04e6f2a0b9b2a63c80e9497022c5a5758674d
zoulixin93 <31400800+zoulixin93@users.noreply.github.com>
2022-10-19 13:11:27 +0800

    Update unbiased_learning.py

--- unbiased_learning.py
+++ unbiased_learning.py
@@ -105,7 +105,7 @@ for train_batch in train_data_loader:
             f'pnr {result_dict_click["pnr"]:.6f}'
         )
         if idx % config.save_step == 0 and idx > 0:
-            paddle.save(model.state_dict(),
+            torch.save(model.state_dict(),
                       'save_model/save_steps{}_{:.5f}_{:5f}.model'.format(idx, result_dict_ann['pnr'], result_dict_click['pnr'])
             )
-    idx += 1
+        idx += 1
